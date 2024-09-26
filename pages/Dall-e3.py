cdb82c77aefa9b50ea79a791874bd49cb55fbd28
lamiss1 <101821279+lamiss1@users.noreply.github.com>
2024-09-26 15:30:56 +0200

    Update Dall-e3.py

--- pages/Dall-e3.py
+++ pages/Dall-e3.py
@@ -3,6 +3,30 @@ import streamlit as st
 st.title("Dall-e3")
 st.write ("Veuillez entrez une description de l´image ")
 
-Description = st.text_input("tapez votre texte")
+Description = st.text_input("tapez votre description")
+
+
+# OpenAI pour générer des images
+from openai import OpenAI
+
+
+client = OpenAI(api_key=OpenAIKEY)
+
+
+# Testez ici plusieurs variation du prompte
+prompt = Description
+
+
+
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=prompt,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+
+image_url = image.data[0].url
+print(image_url)
 
 
